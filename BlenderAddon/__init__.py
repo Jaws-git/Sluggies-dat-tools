4f831bae2f0a1b33dd3096fa365a1e5bc8f69f0f
JawSFM <gernos28-git@yahoo.de>
2026-04-22 18:23:23 +0200

    Add barebones Blender addon

---
+++ BlenderAddon/__init__.py
@@ -0,0 +1,34 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import bpy
+from . import ImportSluggies
+
+bl_info = {
+    "name": "Sluggies IO",
+    "author": "JawSFM",
+    "description": "Import Sluggers intermediate JSON files",
+    "blender": (4, 2, 0),
+    "version": (0, 0, 1),
+    "location": "File > Import > Sluggers intermediate (.json)",
+    "warning": "",
+    "category": "Import-Export",
+}
+
+
+def register():
+    ImportSluggies.register()
+
+
+def unregister():
+    ImportSluggies.unregister()
